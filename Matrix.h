8b22e1ac40a1f6987bb377f1361c7645af4e2f80
Janos Maros <jmaros@t-online.hu>
2024-02-18 02:21:47 +0100

    Tabs to spaces for GitHub's sake!

--- Matrix.h
+++ Matrix.h
@@ -11,209 +11,209 @@ using std::size_t;
 using std::vector;
 
 namespace LinAlg {
-	
-	using std::shared_ptr;
+    
+    using std::shared_ptr;
 
 // declarations
 
-	// Single row of the matrix
-	template <typename T>
-	class Row {
-	public:
-	// constructors
-		explicit Row (size_t	numCols,
-					  const T	& initialValue);
+    // Single row of the matrix
+    template <typename T>
+    class Row {
+    public:
+    // constructors
+        explicit Row (size_t	numCols,
+                      const T	& initialValue);
 
-	// accessors
-		inline const T& CRefVal (size_t	numCol) const;
-		inline const T	Value	(size_t	numCol) const;
-		inline size_t	Size	()				const;
-		// operators
-		const T& operator [] (size_t	index) const;
+    // accessors
+        inline const T& CRefVal (size_t	numCol) const;
+        inline const T	Value	(size_t	numCol) const;
+        inline size_t	Size	()				const;
+        // operators
+        const T& operator [] (size_t	index) const;
 
-	// modifyers
-		// operators
-		T& operator [] (size_t	index);
-	private:
-		vector<T>		m_row;
-	};
+    // modifyers
+        // operators
+        T& operator [] (size_t	index);
+    private:
+        vector<T>		m_row;
+    };
 
-	template <typename T> 
-	class Matrix {
-	public:
-		Matrix<T>(size_t		numRows,
-				  size_t		numCols,
-				  const		T&	initialValue = T{});
+    template <typename T> 
+    class Matrix {
+    public:
+        Matrix<T>(size_t		numRows,
+                  size_t		numCols,
+                  const		T&	initialValue = T{});
 
-	// accessors
-		constexpr size_t MinRow () const;
-		constexpr size_t MinCol () const;
-		inline size_t RowSize () const;
-		inline size_t ColSize () const;
-		inline size_t RowUpperLimit () const;
-		inline size_t ColUpperLimit () const;
+    // accessors
+        constexpr size_t MinRow () const;
+        constexpr size_t MinCol () const;
+        inline size_t RowSize () const;
+        inline size_t ColSize () const;
+        inline size_t RowUpperLimit () const;
+        inline size_t ColUpperLimit () const;
 
-		inline const T&	DataCRef (size_t		rowIndex,
-								  size_t		colIndex) const;
+        inline const T&	DataCRef (size_t		rowIndex,
+                                  size_t		colIndex) const;
 
-		inline const T	Value (size_t		rowIndex,
-							   size_t		colIndex) const;
+        inline const T	Value (size_t		rowIndex,
+                               size_t		colIndex) const;
 
-		inline size_t	Size () const;
+        inline size_t	Size () const;
 
-		Matrix<T> CreateTransposed () const;
+        Matrix<T> CreateTransposed () const;
 
-	// operators
-		const Row<T>& operator [] (size_t	index) const;
+    // operators
+        const Row<T>& operator [] (size_t	index) const;
 
-	// modifiers
-		inline T& DataRef (size_t		rowIndex,
-						   size_t		colIndex);
+    // modifiers
+        inline T& DataRef (size_t		rowIndex,
+                           size_t		colIndex);
 
-	// operators
-		Row<T>& operator [] (size_t	index);
+    // operators
+        Row<T>& operator [] (size_t	index);
 
-	private:
-		vector<Row<T>>		m_rows;
-	};
+    private:
+        vector<Row<T>>		m_rows;
+    };
 
 
 // definitions
-	// constructors
-	template <typename T>
-	Row<T>::Row (size_t		numCols,
-				 const T	& initialValue)
-		: m_row			(numCols,
-						 initialValue)
-	{
-	}
+    // constructors
+    template <typename T>
+    Row<T>::Row (size_t		numCols,
+                 const T	& initialValue)
+        : m_row			(numCols,
+                         initialValue)
+    {
+    }
 
-	template <typename T>
-	Matrix<T>::Matrix (size_t	numRows,
-					   size_t	numCols,
-					   const T	& initialValue)
-		: m_rows	(numRows,
-					 Row<T> (numCols,
-							 initialValue))
-	{
-	}
+    template <typename T>
+    Matrix<T>::Matrix (size_t	numRows,
+                       size_t	numCols,
+                       const T	& initialValue)
+        : m_rows	(numRows,
+                     Row<T> (numCols,
+                             initialValue))
+    {
+    }
 
-	// accessors
-	template <typename T>
-	inline const T& Row<T>::CRefVal (size_t		numCol) const
-	{
-		return m_row[numCol];
-	}
+    // accessors
+    template <typename T>
+    inline const T& Row<T>::CRefVal (size_t		numCol) const
+    {
+        return m_row[numCol];
+    }
 
-	template <typename T>
-	inline const T Row<T>::Value (size_t		numCol) const
-	{
-		return m_row[numCol];
-	}
+    template <typename T>
+    inline const T Row<T>::Value (size_t		numCol) const
+    {
+        return m_row[numCol];
+    }
 
-	template <typename T>
-	inline size_t Row<T>::Size () const
-	{
-		return m_row.size();
-	}
+    template <typename T>
+    inline size_t Row<T>::Size () const
+    {
+        return m_row.size();
+    }
 
-	template <typename T>
-	constexpr size_t  Matrix<T>::MinRow () const
-	{
-		return 0u;
-	}
+    template <typename T>
+    constexpr size_t  Matrix<T>::MinRow () const
+    {
+        return 0u;
+    }
 
-	template <typename T>
-	constexpr size_t  Matrix<T>::MinCol () const
-	{
-		return 0u;
-	}
+    template <typename T>
+    constexpr size_t  Matrix<T>::MinCol () const
+    {
+        return 0u;
+    }
 
-	template <typename T>
-	inline size_t  Matrix<T>::RowSize () const
-	{
-		return m_rows.size();
-	}
+    template <typename T>
+    inline size_t  Matrix<T>::RowSize () const
+    {
+        return m_rows.size();
+    }
 
-	template <typename T>
-	inline size_t  Matrix<T>::ColSize () const
-	{
-		return (RowSize() > 0) ? m_rows[0].Size() : 0;
-	}
+    template <typename T>
+    inline size_t  Matrix<T>::ColSize () const
+    {
+        return (RowSize() > 0) ? m_rows[0].Size() : 0;
+    }
 
-	template <typename T>
-	inline size_t  Matrix<T>::RowUpperLimit () const
-	{
-		return RowSize() + MinRow();
-	}
+    template <typename T>
+    inline size_t  Matrix<T>::RowUpperLimit () const
+    {
+        return RowSize() + MinRow();
+    }
 
-	template <typename T>
-	inline size_t  Matrix<T>::ColUpperLimit () const
-	{
-		return ColSize() + MinCol();
-	}
+    template <typename T>
+    inline size_t  Matrix<T>::ColUpperLimit () const
+    {
+        return ColSize() + MinCol();
+    }
 
-	// preferred data access for non POD typenames
-	template <typename T>
-	inline const T& Matrix<T>::DataCRef(size_t		rowIndex,
-										size_t		colIndex) const
-	{
-		return m_rows[rowIndex][colIndex];
-	}
+    // preferred data access for non POD typenames
+    template <typename T>
+    inline const T& Matrix<T>::DataCRef(size_t		rowIndex,
+                                        size_t		colIndex) const
+    {
+        return m_rows[rowIndex][colIndex];
+    }
 
-	template <typename T>
-	inline const T	Matrix<T>::Value (size_t		rowIndex,
-									  size_t		colIndex) const
-	{
-		return m_rows[rowIndex][colIndex];
-	}
+    template <typename T>
+    inline const T	Matrix<T>::Value (size_t		rowIndex,
+                                      size_t		colIndex) const
+    {
+        return m_rows[rowIndex][colIndex];
+    }
 
-	// operators
-	template <typename T>
-	const T& Row<T>::operator [] (size_t	index) const
-	{
-		return m_row[index];
-	}
+    // operators
+    template <typename T>
+    const T& Row<T>::operator [] (size_t	index) const
+    {
+        return m_row[index];
+    }
 
-	template <typename T>
-	const Row<T>& Matrix<T>::operator [] (size_t	rowIndex) const
-	{
-		return m_rows[rowIndex];
-	}
+    template <typename T>
+    const Row<T>& Matrix<T>::operator [] (size_t	rowIndex) const
+    {
+        return m_rows[rowIndex];
+    }
 
-	template <typename T>
-	Matrix<T> Matrix<T>::CreateTransposed () const
-	{
-		static_assert(MinRow() == 0 && MinCol() == 0, "Unexpected configuration!");
-		Matrix<T> transposed(ColSize(), RowSize());
-		for (auto rowIndex = MinRow(); rowIndex < RowUpperLimit(); ++rowIndex) {
-			for (auto colIndex = MinCol(); colIndex < ColUpperLimit(); ++colIndex) {
-				transposed[colIndex][rowIndex] = Value(rowIndex, colIndex);
-			}
-		}
-		return transposed;
-	}
+    template <typename T>
+    Matrix<T> Matrix<T>::CreateTransposed () const
+    {
+        static_assert(MinRow() == 0 && MinCol() == 0, "Unexpected configuration!");
+        Matrix<T> transposed(ColSize(), RowSize());
+        for (auto rowIndex = MinRow(); rowIndex < RowUpperLimit(); ++rowIndex) {
+            for (auto colIndex = MinCol(); colIndex < ColUpperLimit(); ++colIndex) {
+                transposed[colIndex][rowIndex] = Value(rowIndex, colIndex);
+            }
+        }
+        return transposed;
+    }
 
 // modifiers
-	template <typename T>
-	inline T& Matrix<T>::DataRef (size_t		rowIndex,
-								  size_t		colIndex)
-	{
-		return m_rows[rowIndex][colIndex];
-	}
+    template <typename T>
+    inline T& Matrix<T>::DataRef (size_t		rowIndex,
+                                  size_t		colIndex)
+    {
+        return m_rows[rowIndex][colIndex];
+    }
 
-	// operators
-	template <typename T>
-	T& Row<T>::operator [] (size_t	index)
-	{
-		return m_row[index];
-	}
+    // operators
+    template <typename T>
+    T& Row<T>::operator [] (size_t	index)
+    {
+        return m_row[index];
+    }
 
-	template <typename T>
-	Row<T>& Matrix<T>::operator [] (size_t	rowIndex)
-	{
-		return m_rows[rowIndex];
-	}
+    template <typename T>
+    Row<T>& Matrix<T>::operator [] (size_t	rowIndex)
+    {
+        return m_rows[rowIndex];
+    }
 
 } // namespace LinAlg
 
--- Tesselation.cpp
+++ Tesselation.cpp
@@ -14,187 +14,187 @@ using std::string;
 using std::stringstream;
 
 namespace Nessie {
-	using std::map;
+    using std::map;
 
-	const map<MonthEnum, int> MonthDays {
-		{ MonthEnum::January,	31 },
-		{ MonthEnum::LeapFebr,	29 },
-		{ MonthEnum::February,	28 },
-		{ MonthEnum::March,		31 },
-		{ MonthEnum::April,		30 },
-		{ MonthEnum::May,		31 },
-		{ MonthEnum::June,		30 },
-		{ MonthEnum::July,		31 },
-		{ MonthEnum::August,	31 },
-		{ MonthEnum::September,	30 },
-		{ MonthEnum::October,	31 },
-		{ MonthEnum::November,	30 },
-		{ MonthEnum::December,	31 }
-	};
+    const map<MonthEnum, int> MonthDays {
+        { MonthEnum::January,	31 },
+        { MonthEnum::LeapFebr,	29 },
+        { MonthEnum::February,	28 },
+        { MonthEnum::March,		31 },
+        { MonthEnum::April,		30 },
+        { MonthEnum::May,		31 },
+        { MonthEnum::June,		30 },
+        { MonthEnum::July,		31 },
+        { MonthEnum::August,	31 },
+        { MonthEnum::September,	30 },
+        { MonthEnum::October,	31 },
+        { MonthEnum::November,	30 },
+        { MonthEnum::December,	31 }
+    };
 
-	const map<MonthEnum, string> MonthNames {
-		{ MonthEnum::Unknown,	"Undisclosed"	},
-		{ MonthEnum::January,	"January"		},
-		{ MonthEnum::LeapFebr,	"February"		},
-		{ MonthEnum::February,	"February"		},
-		{ MonthEnum::March,		"March"			},
-		{ MonthEnum::April,		"April"			},
-		{ MonthEnum::May,		"May"			},
-		{ MonthEnum::June,		"June"			},
-		{ MonthEnum::July,		"July"			},
-		{ MonthEnum::August,	"August"		},
-		{ MonthEnum::September,	"September"		},
-		{ MonthEnum::October,	"October"		},
-		{ MonthEnum::November,	"November"		},
-		{ MonthEnum::December,	"December"		}
-	};
+    const map<MonthEnum, string> MonthNames {
+        { MonthEnum::Unknown,	"Undisclosed"	},
+        { MonthEnum::January,	"January"		},
+        { MonthEnum::LeapFebr,	"February"		},
+        { MonthEnum::February,	"February"		},
+        { MonthEnum::March,		"March"			},
+        { MonthEnum::April,		"April"			},
+        { MonthEnum::May,		"May"			},
+        { MonthEnum::June,		"June"			},
+        { MonthEnum::July,		"July"			},
+        { MonthEnum::August,	"August"		},
+        { MonthEnum::September,	"September"		},
+        { MonthEnum::October,	"October"		},
+        { MonthEnum::November,	"November"		},
+        { MonthEnum::December,	"December"		}
+    };
 
-	// standalone helper functions
-	string NameOfMonth (MonthEnum	month)
-	{
-		string mnStr;
-		auto mnmCit = MonthNames.find(month);
-		if (mnmCit != MonthNames.end()) {
-			mnStr = mnmCit->second;
-		} else {
-			mnStr = "***No Such month!***";
-		}
-		return mnStr;
-	}
+    // standalone helper functions
+    string NameOfMonth (MonthEnum	month)
+    {
+        string mnStr;
+        auto mnmCit = MonthNames.find(month);
+        if (mnmCit != MonthNames.end()) {
+            mnStr = mnmCit->second;
+        } else {
+            mnStr = "***No Such month!***";
+        }
+        return mnStr;
+    }
 
-	string DateStr (MonthEnum	month,
-					int		day)
-	{
-		stringstream dateStream;
-		dateStream << NameOfMonth(month) << " " << day;
-		return dateStream.str();
-	}
+    string DateStr (MonthEnum	month,
+                    int		day)
+    {
+        stringstream dateStream;
+        dateStream << NameOfMonth(month) << " " << day;
+        return dateStream.str();
+    }
 
-	bool IsValidDate (MonthEnum		month,
-					  int		day)
-	{
-		bool isValid{};
-		auto mldCit = MonthDays.find(month);
-		if (mldCit != MonthDays.end()) {
-			isValid = mldCit->second >= day;
-		}
-		return isValid;
-	}
+    bool IsValidDate (MonthEnum		month,
+                      int		day)
+    {
+        bool isValid{};
+        auto mldCit = MonthDays.find(month);
+        if (mldCit != MonthDays.end()) {
+            isValid = mldCit->second >= day;
+        }
+        return isValid;
+    }
 
-	Tesselation::Tesselation (const TableLayout			& table,
-							  const ShapeCollection		& shapes,
-							  MonthEnum					month,
-							  int						day)
-	 : m_tableLayout	(table)
-	 , m_shapes			(shapes)
-	 , m_month			(month)
-	 , m_day			(day)
-	 , m_tableShape		(table.NumRows(),
-						 table.NumCols())
-	{
-		for (size_t row = 0; row < m_tableLayout.NumRows(); ++row) {
-			for (size_t col = 0; col < m_tableLayout.NumRows(); ++col) {
+    Tesselation::Tesselation (const TableLayout			& table,
+                              const ShapeCollection		& shapes,
+                              MonthEnum					month,
+                              int						day)
+     : m_tableLayout	(table)
+     , m_shapes			(shapes)
+     , m_month			(month)
+     , m_day			(day)
+     , m_tableShape		(table.NumRows(),
+                         table.NumCols())
+    {
+        for (size_t row = 0; row < m_tableLayout.NumRows(); ++row) {
+            for (size_t col = 0; col < m_tableLayout.NumRows(); ++col) {
 //				m_tableShape[row][col] = m_tableLayout[row][col];
-			}
-		}
-	}
+            }
+        }
+    }
 
-	MonthEnum Tesselation::Month ()	const
-	{
-		return m_month;
-	}
+    MonthEnum Tesselation::Month ()	const
+    {
+        return m_month;
+    }
 
-	int Tesselation::Day ()	const
-	{
-		return m_day;
-	}
+    int Tesselation::Day ()	const
+    {
+        return m_day;
+    }
 
-		// accessors
-	ostream& operator << (ostream& os,
-						  const Tesselation& tessy)
-	{
-		os << "Tesselation Date = " << DateStr(tessy.Month(),
-											   tessy.Day());
-		return os;
-	}
+        // accessors
+    ostream& operator << (ostream& os,
+                          const Tesselation& tessy)
+    {
+        os << "Tesselation Date = " << DateStr(tessy.Month(),
+                                               tessy.Day());
+        return os;
+    }
 } // namespace Nessie
 
 using namespace Nessie;
 
 int main ()
 {
-	// The zeros representing the excluded parts
-	// The negative numbers are placeholders for the monthes
-	// The positive numbers specify the position of the given day on the table
-	const TableLayout tableLayout = {
-		{  -1,  -2,  -3,  -4,  -5,  -7,   0 },
-		{  -7,  -8,  -9, -10, -11, -12,   0 },
-		{  +1,  +2,  +3,  +4,  +5,  +6,  +7 },
-		{  +8,  +9, +10, +11, +12, +13, +14 },
-		{ +15, +16, +17, +18, +19, +20, +21 },
-		{ +22, +23, +24, +25, +26, +27, +28 },
-		{ +29, +30, +31,   0,   0,   0,   0 }
-	};
+    // The zeros representing the excluded parts
+    // The negative numbers are placeholders for the monthes
+    // The positive numbers specify the position of the given day on the table
+    const TableLayout tableLayout = {
+        {  -1,  -2,  -3,  -4,  -5,  -7,   0 },
+        {  -7,  -8,  -9, -10, -11, -12,   0 },
+        {  +1,  +2,  +3,  +4,  +5,  +6,  +7 },
+        {  +8,  +9, +10, +11, +12, +13, +14 },
+        { +15, +16, +17, +18, +19, +20, +21 },
+        { +22, +23, +24, +25, +26, +27, +28 },
+        { +29, +30, +31,   0,   0,   0,   0 }
+    };
 
-	//const TableShape tableShape = {
-	//	{ 0, 0, 0, 0, 0, 0, 1 },
-	//	{ 0, 0, 0, 0, 0, 0, 1 },
-	//	{ 0, 0, 0, 0, 0, 0, 0 },
-	//	{ 0, 0, 0, 0, 0, 0, 0 },
-	//	{ 0, 0, 0, 0, 0, 0, 0 },
-	//	{ 0, 0, 0, 0, 0, 0, 0 },
-	//	{ 0, 0, 0, 1, 1, 1, 1 }
-	//};
+    //const TableShape tableShape = {
+    //	{ 0, 0, 0, 0, 0, 0, 1 },
+    //	{ 0, 0, 0, 0, 0, 0, 1 },
+    //	{ 0, 0, 0, 0, 0, 0, 0 },
+    //	{ 0, 0, 0, 0, 0, 0, 0 },
+    //	{ 0, 0, 0, 0, 0, 0, 0 },
+    //	{ 0, 0, 0, 0, 0, 0, 0 },
+    //	{ 0, 0, 0, 1, 1, 1, 1 }
+    //};
 
 
-	const ShapeCollection shapes = {
+    const ShapeCollection shapes = {
 
-		{{ 1, 0, 1 },
-		 { 1, 1, 1 }},
-	
-		{{ 1, 1, 1 },
-		 { 1, 1, 1 }},
-	
-		{{ 1, 1, 1 },
-		 { 1, 1, 0 }},
+        {{ 1, 0, 1 },
+         { 1, 1, 1 }},
+    
+        {{ 1, 1, 1 },
+         { 1, 1, 1 }},
+    
+        {{ 1, 1, 1 },
+         { 1, 1, 0 }},
 
-		{{ 1, 0, 0 },
-		 { 1, 0, 0 },
-		 { 1, 1, 1 }},
+        {{ 1, 0, 0 },
+         { 1, 0, 0 },
+         { 1, 1, 1 }},
 
-		{{ 1, 1, 0 },
-		 { 0, 1, 0 },
-		 { 0, 1, 1 }},
+        {{ 1, 1, 0 },
+         { 0, 1, 0 },
+         { 0, 1, 1 }},
 
-		{{ 1, 1, 1, 1 },
-		 { 0, 1, 0, 0 }},
+        {{ 1, 1, 1, 1 },
+         { 0, 1, 0, 0 }},
 
-		{{ 1, 1, 1, 0 },
-		 { 0, 0, 1, 1 }},
+        {{ 1, 1, 1, 0 },
+         { 0, 0, 1, 1 }},
 
-		{{ 1, 1, 1, 1 },
-		 { 1, 0, 0, 0 }}
-	};
+        {{ 1, 1, 1, 1 },
+         { 1, 0, 0, 0 }}
+    };
 
-	MonthEnum	month		= MonthEnum::February;
-	int		day			= 17;
-	bool	isLeapYear	= true;
+    MonthEnum	month		= MonthEnum::February;
+    int		day			= 17;
+    bool	isLeapYear	= true;
 
-	if (isLeapYear && month == MonthEnum::February) {
-		month = MonthEnum::LeapFebr;
-	}
+    if (isLeapYear && month == MonthEnum::February) {
+        month = MonthEnum::LeapFebr;
+    }
 
-	if (IsValidDate(month, day)) {
-		cout << "The date is " << DateStr(month, day) << endl;
-		Tesselation tesselation (tableLayout,
-								 shapes,
-								 month,
-								 day);
-		//tesselation.Solve();
-		cout << tesselation << "\n";
-	} else {
-		cout << "No such date as " << DateStr(month, day) << endl;
-	}
+    if (IsValidDate(month, day)) {
+        cout << "The date is " << DateStr(month, day) << endl;
+        Tesselation tesselation (tableLayout,
+                                 shapes,
+                                 month,
+                                 day);
+        //tesselation.Solve();
+        cout << tesselation << "\n";
+    } else {
+        cout << "No such date as " << DateStr(month, day) << endl;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
