13b2434ca61950695721d1e951c8d4d735e328f0
gyukiss55 <gs.gkiss@gmail.com>
2024-11-08 10:06:17 +0100

    C# version

---
+++ Tile/Pair.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tile
+{
+    internal class Pair<T>
+    {
+        public T Value1 { get; }
+        public T Value2 { get; }
+
+        public Pair(T value1, T value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+
+        public override string ToString()
+        {
+            return $"({Value1}, {Value2})";
+        }
+    }
+}
